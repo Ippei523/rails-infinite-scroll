48fb4fd77adc52bb38c53065344643586b3aca87
Ippei523 <ippei523@gmail.com>
2024-01-27 23:34:24 +0900

    seedにUserを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# userを作成
+# userを100人以上作成したい場合は、以下のように書く
+# すでに存在している場合は、作成しない
+100.times do |n|
+  User.find_or_create_by!(name: "John Doe #{n + 1}")
+end
+
+# 1人だけ作成する場合は、以下のように書く
+# User.find_or_create_by!(name: "John Doe")
